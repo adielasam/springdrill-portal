1067535261e26046cf9bd4679b901ff8a4675cce
adielasam <microsoftportharcourt@gmail.com>
2026-02-23 18:35:08 +0100

    Add files via upload

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,7 @@
-// supabaseClient.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
+// Replace with your actual Project URL and Publishable Key from Supabase Settings
 const supabaseUrl = 'https://jmxyopohngslqvzknjnt.supabase.co';
-// Paste the Publishable key from your screenshot here
 const supabaseKey = 'sb_publishable_yGvqUxGlwiJuSkFN4VOpWw_nKYmo-vl'; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
